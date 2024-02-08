3de2caa76d83bc8207b0a047e928fa6b7862baca
PKG-7 <pavel.k.group@gmail.com>
2024-02-08 09:15:04 +0300

    feat: :sparkles: Working Vercel version

--- src/bot/features/welcome.ts
+++ src/bot/features/welcome.ts
@@ -1,4 +1,4 @@
-import { Composer } from "grammy";
+import { Composer, Keyboard } from "grammy";
 import type { Context } from "#root/bot/context.js";
 import { logHandle } from "#root/bot/helpers/logging.js";
 
@@ -6,8 +6,17 @@ const composer = new Composer<Context>();
 
 const feature = composer.chatType("private");
 
-feature.command("start", logHandle("command-start"), (ctx) => {
-  return ctx.reply(ctx.t("welcome"));
+const keyboard = new Keyboard().text("💰 Test ").row().resized();
+
+feature.command("start", logHandle("command-start"), async (ctx) => {
+  // Define the keyboard layout
+
+  // Reply with a message and the keyboard
+  await ctx.reply(ctx.t("welcome"), {
+    reply_markup: {
+      keyboard: keyboard.build(),
+    },
+  });
 });
 
 export { composer as welcomeFeature };
